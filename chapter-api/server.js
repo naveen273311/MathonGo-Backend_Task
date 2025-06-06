24052d97bc25f74bc89b9fe78f62a4429c1a33cd
naveen273311 <103899742+naveen273311@users.noreply.github.com>
2025-06-06 16:49:32 +0530

    server.js

--- chapter-api/server.js
+++ chapter-api/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { createClient } = require("redis");
 const app = require("./app");
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 const REDIS_URL = process.env.REDIS_URL;
 

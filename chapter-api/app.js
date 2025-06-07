2d4acfafea230b8e65f8c9db68f9b6d8aa7c1d67
naveen273311 <103899742+naveen273311@users.noreply.github.com>
2025-06-07 09:59:34 +0530

    app.js

--- chapter-api/app.js
+++ chapter-api/app.js
@@ -3,22 +3,20 @@ const dotenv = require("dotenv");
 const chapterRoutes = require("./routes/chapterRoutes");
 const limiter = require("./middlewares/rateLimiter");
 
-
 dotenv.config();
 
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Rate limiter applied to chapter routes
 app.use("/api/v1/chapters", limiter, chapterRoutes);
 
 // Test route to check server is working
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-
-// API routes
-app.use("/api/v1/chapters", chapterRoutes);
 
 // Fallback for 404
 app.use((req, res) => {
